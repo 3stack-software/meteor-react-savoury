0387c62d47a00d035331308e72b6b33e9102f7fb
Nathan Muir <ndmuir@gmail.com>
2016-10-27 10:37:10 +1000

    Release 0.0.2 (Compatible with Meteor 1.4)

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: '3stack:react-savoury',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Reactively show the user the status of their method call.',
   git: 'https://github.com/3stack-software/meteor-react-savoury',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api){
-  api.versionsFrom('METEOR@1.2');
+  api.versionsFrom('METEOR@1.4');
   api.use([
     'meteor-base',
-    'react@0.1.13'
+    'react@15.0.1'
   ]);
   api.export([
     'ReactSavoury',
